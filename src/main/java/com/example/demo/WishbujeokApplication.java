1eb4b19086a5ab3c811b66a27d1c6ff95964fe29
KCH <doolysmile1@naver.com>
2022-11-29 21:27:51 +0900

    initial settings

---
+++ src/main/java/com/example/demo/WishbujeokApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishbujeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishbujeokApplication.class, args);
+	}
+
+}
